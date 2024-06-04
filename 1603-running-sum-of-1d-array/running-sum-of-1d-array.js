d13308d0bf4809ea20a8ab634ea4d22b5da7463f
HarshilTandel <160136220+HarshilTandel@users.noreply.github.com>
2024-06-04 21:51:50 +0530

    Time: 60 ms (41.77%) | Memory: 51.1 MB (43.94%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+        let ans=0;
+    let farr=[];
+    for(i=0;i<nums.length;i++){
+        ans=ans+nums[i];
+        farr.push(ans);
+    }
+    return farr;
+};
