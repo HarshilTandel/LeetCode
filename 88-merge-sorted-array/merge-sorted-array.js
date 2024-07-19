6207362c57e76decca62a9446bbcb9c9fac95fab
HarshilTandel <160136220+HarshilTandel@users.noreply.github.com>
2024-07-19 11:29:07 +0530

    Time: 54 ms (53.94%) | Memory: 49.1 MB (30.04%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,25 +6,15 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-        let index1 = m - 1;
-    let index2 = n - 1;
-    let index_merged = m + n - 1;
-
-    while (index1 >= 0 && index2 >= 0) {
-        if (nums1[index1] > nums2[index2]) {
-            nums1[index_merged] = nums1[index1];
-            index1--;
-        } else {
-            nums1[index_merged] = nums2[index2];
-            index2--;
-        }
-        index_merged--;
+    let pointer1 = m-1;
+    let pointer2 = n-1;
+    let i=m+n-1;
+    while(pointer2>=0){
+    if(pointer1>=0 && nums1[pointer1]>nums2[pointer2]){
+        nums1[i--] = nums1[pointer1--];
+    }else{
+        nums1[i--]=nums2[pointer2--];
     }
-
-
-    while (index2 >= 0) {
-        nums1[index_merged] = nums2[index2];
-        index2--;
-        index_merged--;
-    }
+}
+return nums1;
 };
