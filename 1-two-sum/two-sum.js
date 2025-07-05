98037a0eb882fe5198d228676d0148124fac0242
HarshilTandel <160136220+HarshilTandel@users.noreply.github.com>
2025-07-05 15:30:35 +0530

    Time: 5 ms (50.21%) | Memory: 53.9 MB (84.14%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-        for(i=0;i<nums.length;i++){
-        for(j=0;j<nums.length;j++){
-            if(i==j){
-                continue;
-            }else if(nums[i]+nums[j]==target){
-                return [i,j];
-            }
+    // Map to store value → its index
+    const indexMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        
+        // If complement already seen, we have our answer
+        if (indexMap.has(complement)) {
+            return [ indexMap.get(complement), i ];
         }
+        
+        // Otherwise, record this number’s index
+        indexMap.set(nums[i], i);
     }
-    return i,j;
+    
+    // According to problem constraints, there is always exactly one solution,
+    // so we never actually reach here in valid inputs.
+    return [];
 };
