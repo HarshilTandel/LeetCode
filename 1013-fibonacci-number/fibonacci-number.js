5d249226e037e265ddc32fc551a2c9f21d87a498
HarshilTandel <160136220+HarshilTandel@users.noreply.github.com>
2025-07-14 22:38:19 +0530

    Time: 60 ms (43.06%) | Memory: 53.7 MB (28.60%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,10 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    let arr = [0,1];
-    for(let i = 2; i <= n; i++){
-        arr.push(arr[i-2] +arr[i-1])
-    }
-    return arr[n];
+var fib = function (n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
 };
