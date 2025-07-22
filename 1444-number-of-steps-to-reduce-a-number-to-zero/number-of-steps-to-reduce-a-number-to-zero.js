b83315503b4b38700238168490ef4bff83667e05
HarshilTandel <160136220+HarshilTandel@users.noreply.github.com>
2025-07-22 21:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (77.85%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var numberOfSteps = function(num) {
-    let steps=0;
-    while(num>0){
-    if(num%2===0){
-        num=num/2; 
-    }else{
-        num=num-1;  
+        let steps = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num = num / 2;
+        } else {
+            num = num - 1;
+        }
+        steps++;
     }
-   steps++;
-}
-return steps;
+    return steps;
 };
